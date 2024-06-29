c75ed34f55369f76676c4b43e6064dc0a28bb3db
Sean Ryan <strya2@student.monash.edu>
2024-06-29 11:04:12 +1000

    2192 Completed

--- 2192_M_DirectedAcyclicGraph.py
+++ 2192_M_DirectedAcyclicGraph.py
@@ -1,21 +1,16 @@
+# 29/June/2024 
 from typing import List
 
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         # Initialize the answer list with n empty lists
         answer = [[] for _ in range(n)]
-        # print("Answer = ", answer)
         
         # Identify Connections
-        # print("Edges")
-        # print(edges)
         for x in range(len(edges)):
-            # print("Test for", edges[x])
-            # print("Add ", edges[x][0], " to ", edges[x][1])
             answer[edges[x][1]].append(edges[x][0])
-        # print(answer)
 
-        # Add missing values (transitive ancestors)
+        # Add missing values -ancestors
         def find_ancestors(node, ancestors):
             for ancestor in answer[node]:
                 if ancestor not in ancestors:
@@ -23,14 +18,11 @@ class Solution:
                     find_ancestors(ancestor, ancestors)
 
 
-        # Sort
-        # print("Sorting")
+        # Sort and apply ancestors
         for x in range(n):
             ancestors = set()
             find_ancestors(x, ancestors)
             answer[x] = sorted(ancestors)
-        
-        # print(answer)
         
         return answer
 
